ce0618b4c02bc32bdc76d2a22f25efff744ee689
VK <vk006@mail.ru>
2026-06-02 12:31:29 +0300

    Test step

---
+++ Test_Explicit_Waits_2.4.8.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+    )
+    button = browser.find_element(By.ID, "book")
+    button.click()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+    button = browser.find_element(By.ID, "solve")
+    button.click()
+
+    # ждем загрузки страницы
+    time.sleep(5)
+finally:
+    # успеваем скопировать код за 10 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
